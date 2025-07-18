01307e5d31cc8d4fb703275121d701778f200a45
Unstop GitHub Commands <mahtomayuri@gmail.com>
2025-07-18 13:18:10 +0530

    UpdateDesignation

--- WEB-INF/classes/com/web/application/hr/servlets/UpdateDesignation.java
+++ WEB-INF/classes/com/web/application/hr/servlets/UpdateDesignation.java
@@ -2,39 +2,105 @@ package com.web.application.hr.servlets;
 import com.web.application.hr.dl.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-import java.io.*;
 import java.sql.*;
+import java.io.*;
 import java.util.*;
 
-public class EditDesignation extends HttpServlet
+public class UpdateDesignation extends HttpServlet
 {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 	{
+		PrintWriter pw=null;
+		String title="";
+		int code=0;
 		try
 		{
-			int code=0;
+			pw=response.getWriter();
+			title=request.getParameter("title");
+			if(title==null)
+			{
+				sendBackView(response);
+				return;
+			}
+
 			try
 			{
 				code=Integer.parseInt(request.getParameter("code"));
 			}
-			catch(NumberFormatException number)
+			catch(NumberFormatException nfe)
 			{
-				//send back view page
-				//address bar m hi agar kuch galat salat code karde toh
-				//designationsView
-
-
 				sendBackView(response);
 				return;
 			}
+			DesignationDTO designation=new DesignationDTO();
+			designation.setCode(code);
+			designation.setTitle(title);
 			DesignationDAO designationDAO=new DesignationDAO();
-			try
-			{
-				DesignationDTO designationDTO=designationDAO.getByCode(code);
-				PrintWriter pw=response.getWriter();
-				response.setContentType("text/html");
+			designationDAO.update(designation);
+			//success case
+			//copying code of AddDesignation.java with some changes
 
-				pw.println("<!DOCTYPE HTML>");
+			pw.println("<!DOCTYPE HTML>");
+			pw.println("<html>");
+			pw.println("<head>");
+			pw.println("<title>HR Application</title>");
+			pw.println("</head>");
+			pw.println("<body>");
+			pw.println("<!-- Main container starts here -->");
+			pw.println("<div style='width:90hw;height:auto;border:1px solid black'>");
+
+			pw.println("<!-- header starts here -->");
+			pw.println("<div style='margin:5px;width:90hw;height:auto;border:1px solid black'>");
+			pw.println("<img src='/WebApplication1/images/logo.png' style='float:left;width:7hw;height:7vh'>");
+			pw.println("<div style='margin-top:9px;margin-bottom:9px;font-size:20pt'>HR Application</div>");
+			pw.println("</div>");
+			pw.println("<!-- header ends here-->");
+
+			pw.println("<!--content-section starts here -->");
+			pw.println("<div style='width:90hw;height:70vh;margin:5px;border:1px solid white'>");
+
+			pw.println("<!--left panel starts here -->");
+			pw.println("<div style='height:65vh;margin:5px;float:left;padding:5px;border:1px solid black'>");
+			pw.println("<a href='/WebApplication1/designationsView'>Designations</a><br>");
+			pw.println("<a href='/WebApplication1/employeesView'>Employees</a><br>");
+			pw.println("<a href='/WebApplication1/index.html'>Home</a>");
+			pw.println("</div>");
+			pw.println("<!--left panel ends here -->");
+
+
+			pw.println("<!--right panel starts here -->");
+			pw.println("<div style='height:65vh;margin-left:105px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;border:1px solid black'>");
+		
+
+			pw.println("<h3>Notification</h3>");
+			pw.println("Designation Updated<br>");
+			pw.println("<form action='/WebApplication1/designationsView'>");
+			pw.println("<button type='submit'>Ok</button>");
+			pw.println("</form>");
+
+			pw.println("</div>");
+			pw.println("<!-- right panel ends here -->");
+
+			pw.println("</div>");
+			pw.println("<!--content-section ends here-->");
+
+			pw.println("<!--footer starts here -->");
+			pw.println("<div style='width:90hw;height:auto;margin:5px;text-align:center;border:1px solid white'>&copy; Mayuri Mahto 2025</div>");
+			pw.println("<!--footer ends here-->");
+
+			pw.println("</div>"); 
+			pw.println("<!--Main container ends here-->");
+			pw.println("</body>");
+			pw.println("</html>");
+		}
+		catch(DAOException daoException)
+		{
+			//new code here of update
+			//send back the form to update with error message
+			//for example the title that we want to update is already existing in the db
+			//copying code of EditDesignation.java
+
+			pw.println("<!DOCTYPE HTML>");
 				pw.println("<html>");
 				pw.println("<head>");
 				pw.println("<title>HR Application</title>");
@@ -93,7 +159,7 @@ public class EditDesignation extends HttpServlet
 
 				pw.println("<input type='hidden' id='code' name='code' value='"+code+"'>");
 
-				pw.println("<input type='text' id='title' name='title' maxLength='35' size='36' value='"+designationDTO.getTitle()+"'>");
+				pw.println("<input type='text' id='title' name='title' maxLength='35' size='36' value='"+title+"'>");
 				pw.println("<span id='titleErrorSection' style='color:red'></span><br>");
 
 				pw.println("<button type='submit'>Update</button>");
@@ -121,23 +187,14 @@ public class EditDesignation extends HttpServlet
 				pw.println("</body>");
 				pw.println("</html>");
 				pw.println("}");
-			}
-			catch(DAOException daoException)
-			{
-				//send back view page
-				// when getByCode is called and code is not found
-				//designationsView
-				
 
-				sendBackView(response);
-				return;
-			}
-		}catch(Exception exception)
+			
+		}
+		catch(Exception exception)
 		{
 			System.out.println(exception.getMessage());
 		}
 	}
-
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 	{
