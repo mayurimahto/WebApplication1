b6dda84454736003160df58e2e2f06978b5bbb76
Unstop GitHub Commands <mahtomayuri@gmail.com>
2025-08-21 17:43:25 +0530

    EmployeeDAO

--- WEB-INF/classes/com/web/application/hr/dl/EmployeeDAO.java
+++ WEB-INF/classes/com/web/application/hr/dl/EmployeeDAO.java
@@ -414,7 +414,7 @@ public class EmployeeDAO
 		{
 			Connection connection=DAOConnection.getConnection();
 			PreparedStatement preparedStatement;
-			preparedStatement=connection.prepareStatement("select employee.id,employee.name,employee.designation_code,designation.title,employee.date_of_birth,employee.gender,employee.is_indian,employee.basic_salary,employee.pan_number,employee.aadhar_card_number from employee inner join designation on employee.designation_code=designation.cose and pan_number=?");
+			preparedStatement=connection.prepareStatement("select employee.id,employee.name,employee.designation_code,designation.title,employee.date_of_birth,employee.gender,employee.is_indian,employee.basic_salary,employee.pan_number,employee.aadhar_card_number from employee inner join designation on employee.designation_code=designation.code and pan_number=?");
 
 			preparedStatement.setString(1,panNumber);
 			ResultSet resultSet=preparedStatement.executeQuery();
@@ -471,4 +471,68 @@ public class EmployeeDAO
 		return employeeDTO;
 	}
 
+	
+	public EmployeeDTO getByAadharCardNumber(String aadharCardNumber) throws DAOException
+	{
+		EmployeeDTO employeeDTO=null;
+		try
+		{
+			Connection connection=DAOConnection.getConnection();
+			PreparedStatement preparedStatement;
+			preparedStatement=connection.prepareStatement("select employee.id,employee.name,employee.designation_code,designation.title,employee.date_of_birth,employee.gender,employee.is_indian,employee.basic_salary,employee.pan_number,employee.aadhar_card_number from employee inner join designation on employee.designation_code=designation.code and aadhar_card_number=?");
+
+			preparedStatement.setString(1,aadharCardNumber);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			if(resultSet.next()==false)
+			{
+				resultSet.close();
+				preparedStatement.close();
+				connection.close();
+				throw new DAOException("Invalid PAN number : "+aadharCardNumber);
+			}
+			int id;
+			String name;
+			int designationCode;
+			String title;
+			java.sql.Date dateOfBirth;
+			String gender;
+			boolean isIndian;
+			BigDecimal basicSalary;
+			String panNumber;
+
+			id=resultSet.getInt("id");
+			name=resultSet.getString("name").trim();
+			designationCode=resultSet.getInt("designation_code");
+			title=resultSet.getString("title").trim();
+			dateOfBirth=resultSet.getDate("date_of_birth");
+			gender=resultSet.getString("gender");
+			isIndian=resultSet.getBoolean("is_indian");
+			basicSalary=resultSet.getBigDecimal("basic_salary");
+			panNumber=resultSet.getString("pan_number").trim();
+		aadharCardNumber=resultSet.getString("aadhar_card_number").trim();
+
+			employeeDTO=new EmployeeDTO();
+			employeeDTO.setEmployeeId("A"+id);
+			employeeDTO.setName(name);
+			employeeDTO.setDesignationCode(designationCode);
+			employeeDTO.setDesignation(title);
+			employeeDTO.setDateOfBirth(dateOfBirth);
+			employeeDTO.setGender(gender);
+			employeeDTO.setIsIndian(isIndian);
+			employeeDTO.setBasicSalary(basicSalary);
+			employeeDTO.setPANNumber(panNumber);
+			employeeDTO.setAadharCardNumber(aadharCardNumber);
+
+
+			resultSet.close();
+			preparedStatement.close();
+			connection.close();
+			
+		}
+		catch(Exception exception)
+		{
+			throw new DAOException(exception.getMessage());
+		}
+		return employeeDTO;
+	}
 }
