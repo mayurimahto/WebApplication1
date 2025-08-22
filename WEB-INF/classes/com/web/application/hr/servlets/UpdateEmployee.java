bb86f024efa93a9e1629718035f1f76c3c732365
Unstop GitHub Commands <mahtomayuri@gmail.com>
2025-08-22 20:36:35 +0530

    UpdateEmployee

--- WEB-INF/classes/com/web/application/hr/servlets/UpdateEmployee.java
+++ WEB-INF/classes/com/web/application/hr/servlets/UpdateEmployee.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 
-public class AddEmployee extends HttpServlet
+public class UpdateEmployee extends HttpServlet
 {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 	{
@@ -17,6 +17,8 @@ public class AddEmployee extends HttpServlet
 			PrintWriter pw=response.getWriter();
 			SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
 			
+
+			String employeeId=request.getParameter("employeeId");
 			String name=request.getParameter("name");
 			int designationCode=Integer.parseInt(request.getParameter("designationCode"));
 			Date dateOfBirth=simpleDateFormat.parse(request.getParameter("dateOfBirth"));
@@ -36,14 +38,57 @@ public class AddEmployee extends HttpServlet
 			EmployeeDAO employeeDAO=new EmployeeDAO();
 			try
 			{
+
+					if(employeeDAO.employeeIdExists(employeeId)==false)
+	{
+		sendBackView(response);
+		return;
+	}
+
+
+
+
 				boolean designationCodeExists=designationDAO.designationCodeExists(designationCode);
-				boolean panNumberExists=employeeDAO.panNumberExists(panNumber);
-				boolean aadharCardNumberExists=employeeDAO.aadharCardNumberExists(aadharCardNumber);
+
+				//panNumber and aadharCardNumber agar kisi aur employeeId ke against exist kar rha h toh woh hum change nhi kar sakte h
+
+				
+				boolean panNumberExists=false;
+				EmployeeDTO employeeDTO;
+				try
+				{
+					employeeDTO=employeeDAO.getByPANNumber(panNumber);
+					if(employeeDTO.getEmployeeId().equalsIgnoreCase(employeeId)==false)
+					{
+						panNumberExists=true;
+					}
+				}
+				catch(DAOException daoException)
+				{
+					panNumberExists=false;
+				}
+
+				
+				boolean aadharCardNumberExists=false;
+				try
+				{
+					employeeDTO=employeeDAO.getByAadharCardNumber(aadharCardNumber);
+					if(employeeDTO.getEmployeeId().equalsIgnoreCase(employeeId)==false)
+					{
+						aadharCardNumberExists=true;
+					}
+				}
+				catch(DAOException daoException)
+				{
+					aadharCardNumberExists=false;
+				}
+	
+
+
 				if(designationCodeExists==false || panNumberExists==true || aadharCardNumberExists==true)
 				{
 				//send back form with proper error message
-				//copy code of GetEmployeeAddForm with few changes
-				//add form resent because of validation problems
+				//edit form resent because of validation problems
 				//also need to ensure that all fields are filled with the data that user fed
 
 
@@ -167,9 +212,9 @@ pw.println("");
 pw.println("return valid;");
 pw.println("}");
 pw.println("");
-pw.println("function cancelAddition()");
+pw.println("function cancelUpdate()");
 pw.println("{");
-pw.println("document.getElementById('cancelAdditionForm').submit();");
+pw.println("document.getElementById('cancelUpdateForm').submit();");
 pw.println("}");
 pw.println("</script>");
 pw.println("</head>");
@@ -198,9 +243,14 @@ pw.println("");
 pw.println("");
 pw.println("<!--right panel starts here -->");
 pw.println("<div style='height:65vh;margin-left:105px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;border:1px solid black'>");
-pw.println("<h2>Employee (Add Module)</h2>");
+pw.println("<h2>Employee (Edit Module)</h2>");
 pw.println("");
-pw.println("<form method='post' action='/WebApplication1/addEmployee' onsubmit='return validateForm(this)'>");
+pw.println("<form method='post' action='/WebApplication1/updateEmployee' onsubmit='return validateForm(this)'>");
+
+
+pw.println("<input type='hidden' id='employeeId' name='employeeId' value='"+employeeId+"'>");
+
+
 pw.println("");
 pw.println("");
 pw.println("<table>");
@@ -377,10 +427,10 @@ pw.println("</tr>");
 pw.println("");
 pw.println("<tr>");
 pw.println("<td>");
-pw.println("<button type='submit'>Add</button>");
+pw.println("<button type='submit'>Update</button>");
 pw.println("</td>");
 pw.println("<td>");
-pw.println("<button type='Button' onclick='cancelAddition()'>Cancel</button>");
+pw.println("<button type='Button' onclick='cancelUpdate()'>Cancel</button>");
 pw.println("</td>");
 pw.println("</tr>");
 pw.println("</table>");
@@ -399,17 +449,23 @@ pw.println("");
 pw.println("</div> ");
 pw.println("<!--Main container ends here-->");
 pw.println("");
-pw.println("<form action='/WebApplication1/employeesView' id='cancelAdditionForm'>");
+pw.println("<form action='/WebApplication1/employeesView' id='cancelUpdateForm'>");
 pw.println("</form>");
 pw.println("</body>");
 pw.println("</html>");
-			
-				}
+return; //edit form resent because of problems
+}	
+}catch(DAOException daoException)
+{
+	System.out.println(daoException);
+}			
 
-				else
-				{
 
 					EmployeeDTO employee=new EmployeeDTO();
+
+
+					employee.setEmployeeId(employeeId);
+
 					employee.setName(name);
 					employee.setDesignationCode(designationCode);
 					employee.setDateOfBirth(dateOfBirth);
@@ -421,88 +477,71 @@ pw.println("</html>");
 
 			try
 			{
-				employeeDAO.add(employee);
-				//the following code has been picked up from AddDesignation code with few changes
-				pw.println("<!DOCTYPE HTML>");
-				pw.println("<html>");
-				pw.println("<head>");
-				pw.println("<title>HR Application</title>");
-				pw.println("</head>");
-				pw.println("<body>");
-				pw.println("<!-- Main container starts here -->");
-				pw.println("<div style='width:90hw;height:auto;border:1px solid black'>");
+				employeeDAO.update(employee);
+				//the following code has been picked up from UpdateDesignation code with few changes
+				
 
-				pw.println("<!-- header starts here -->");
-				pw.println("<div style='margin:5px;width:90hw;height:auto;border:1px solid black'>");
-				pw.println("<img src='/WebApplication1/images/logo.png' style='float:left;width:7hw;height:7vh'>");
-				pw.println("<div style='margin-top:9px;margin-bottom:9px;font-size:20pt'>HR Application</div>");
-				pw.println("</div>");
-				pw.println("<!-- header ends here-->");
+			pw.println("<!DOCTYPE HTML>");
+			pw.println("<html>");
+			pw.println("<head>");
+			pw.println("<title>HR Application</title>");
+			pw.println("</head>");
+			pw.println("<body>");
+			pw.println("<!-- Main container starts here -->");
+			pw.println("<div style='width:90hw;height:auto;border:1px solid black'>");
 
-				pw.println("<!--content-section starts here -->");
-				pw.println("<div style='width:90hw;height:70vh;margin:5px;border:1px solid white'>");
+			pw.println("<!-- header starts here -->");
+			pw.println("<div style='margin:5px;width:90hw;height:auto;border:1px solid black'>");
+			pw.println("<img src='/WebApplication1/images/logo.png' style='float:left;width:7hw;height:7vh'>");
+			pw.println("<div style='margin-top:9px;margin-bottom:9px;font-size:20pt'>HR Application</div>");
+			pw.println("</div>");
+			pw.println("<!-- header ends here-->");
 
-				pw.println("<!--left panel starts here -->");
-				pw.println("<div style='height:65vh;margin:5px;float:left;padding:5px;border:1px solid black'>");
-				pw.println("<a href='/WebApplication1/designationsView'>Designations</a><br>");
-				pw.println("<a href='/WebApplication1/employeesView'>Employees</a><br>");
-				pw.println("<a href='/WebApplication1/index.html'>Home</a>");
-				pw.println("</div>");
-				pw.println("<!--left panel ends here -->");
+			pw.println("<!--content-section starts here -->");
+			pw.println("<div style='width:90hw;height:70vh;margin:5px;border:1px solid white'>");
+
+			pw.println("<!--left panel starts here -->");
+			pw.println("<div style='height:65vh;margin:5px;float:left;padding:5px;border:1px solid black'>");
+			pw.println("<a href='/WebApplication1/designationsView'>Designations</a><br>");
+			pw.println("<a href='/WebApplication1/employeesView'>Employees</a><br>");
+			pw.println("<a href='/WebApplication1/index.html'>Home</a>");
+			pw.println("</div>");
+			pw.println("<!--left panel ends here -->");
 
 
-				pw.println("<!--right panel starts here -->");
-				pw.println("<div style='height:65vh;margin-left:105px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;border:1px solid black'>");
+			pw.println("<!--right panel starts here -->");
+			pw.println("<div style='height:65vh;margin-left:105px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;border:1px solid black'>");
 		
 
-				pw.println("<h3>Notification</h3>");
-				pw.println("Employee added<br>Add more employees?<br>");
-				pw.println("<table>");
-				pw.println("<tr>");
-				pw.println("<td>");
-				pw.println("<form action='/WebApplication1/getEmployeeAddForm'>");
-				pw.println("<button type='submit'>Yes</button>");
-				pw.println("</form>");
-				pw.println("</td>");
-				pw.println("<td>");
-				pw.println("<form action='/WebApplication1/employeesView'>");
-				pw.println("<button type='submit'>No</button>");
-				pw.println("</form>");
-				pw.println("</td>");
-				pw.println("</tr>");
-				pw.println("</table>");
+			pw.println("<h3>Notification</h3>");
+			pw.println("Employee Updated<br>");
+			pw.println("<form action='/WebApplication1/employeesView'>");
+			pw.println("<button type='submit'>Ok</button>");
+			pw.println("</form>");
 
-				pw.println("</div>");
-				pw.println("<!-- right panel ends here -->");
+			pw.println("</div>");
+			pw.println("<!-- right panel ends here -->");
 
-				pw.println("</div>");
-				pw.println("<!--content-section ends here-->");
+			pw.println("</div>");
+			pw.println("<!--content-section ends here-->");
 
-				pw.println("<!--footer starts here -->");
-				pw.println("<div style='width:90hw;height:auto;margin:5px;text-align:center;border:1px solid white'>&copy; Mayuri Mahto 2025</div>");
-				pw.println("<!--footer ends here-->");
+			pw.println("<!--footer starts here -->");
+			pw.println("<div style='width:90hw;height:auto;margin:5px;text-align:center;border:1px solid white'>&copy; Mayuri Mahto 2025</div>");
+			pw.println("<!--footer ends here-->");
 
-				pw.println("</div>"); 
-				pw.println("<!--Main container ends here-->");
-				pw.println("</body>");
-				pw.println("</html>");
+			pw.println("</div>"); 
+			pw.println("<!--Main container ends here-->");
+			pw.println("</body>");
+			pw.println("</html>");
+
 			}
 			catch(DAOException daoException)
 			{
 				//recreate form with error message
 				//and send back the page
 			}
-		
-
-	
-				}
 
  
-			}
-			catch(DAOException daoException)
-			{
-				System.out.println(daoException.getMessage());
-			}
 
 		}
 		catch(Exception exception)
@@ -515,6 +554,207 @@ pw.println("</html>");
 	{
 		
 		doGet(request, response);
+	}
+	
+
+	private void sendBackView(HttpServletResponse response)
+	{
+		try
+		{
+		List<EmployeeDTO>employees=new EmployeeDAO().getAll();
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+pw.println("");
+pw.println("<!DOCTYPE HTML>");
+pw.println("<html>");
+pw.println("<head>");
+pw.println("<title>HR Application</title>");
+pw.println("<script>");
+pw.println("//this is class");
+pw.println("function Employee()");
+pw.println("{");
+pw.println("//properties");
+pw.println("this.employeeId=\"\";");
+pw.println("this.name=\"\";");
+pw.println("this.designationCode=0;");
+pw.println("this.designation=\"\";");
+pw.println("this.dateOfBirth=\"\";");
+pw.println("this.gender=\"\";");
+pw.println("this.isIndian=true;");
+pw.println("this.basicSalary=0;");
+pw.println("this.panNumber=\"\";");
+pw.println("this.aadharCardNumber=\"\";");
+pw.println("}");
+pw.println("");
+pw.println("var employees=[];");
+
+
+SimpleDateFormat simpleDateFormat;
+simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy");
+int i=0;
+for(EmployeeDTO employee:employees)
+{
+pw.println("var employee=new Employee();");
+pw.println("employee.employeeId=\""+employee.getEmployeeId()+"\";");
+pw.println("employee.name=\""+employee.getName()+"\";");
+pw.println("employee.designationCode="+employee.getDesignationCode()+";");
+pw.println("employee.designation=\""+employee.getDesignation()+"\";");
+pw.println("employee.dateOfBirth=\""+simpleDateFormat.format(employee.getDateOfBirth())+"\";");
+pw.println("employee.gender=\""+employee.getGender()+"\";");
+pw.println("employee.isIndian="+employee.getIsIndian()+";");
+pw.println("employee.basicSalary="+employee.getBasicSalary().toPlainString()+";");
+pw.println("employee.panNumber=\""+employee.getPANNumber()+"\";");
+pw.println("employee.aadharCardNumber=\""+employee.getAadharCardNumber()+"\";");
+pw.println("employees["+i+"]=employee;");
+i++;
+}
+
+
+
+pw.println("var selectedRow=null;");
+pw.println("function selectEmployee(row, employeeId)");
+pw.println("{");
+pw.println("//alert(employeeId);");
+pw.println("if(row==selectedRow)return;");
+pw.println("if(selectedRow!=null)");
+pw.println("{");
+pw.println("selectedRow.style.background=\"white\";");
+pw.println("selectedRow.style.color=\"black\";");
+pw.println("}");
+pw.println("row.style.background=\"grey\";");
+pw.println("row.style.color=\"white\";");
+pw.println("selectedRow=row;");
+pw.println("var i;");
+pw.println("for(i=0;i<employees.length;i++)");
+pw.println("{");
+pw.println("if(employees[i].employeeId==employeeId)");
+pw.println("{");
+pw.println("break;");
+pw.println("}");
+pw.println("}");
+pw.println("var emp=employees[i];");
+pw.println("document.getElementById(\"detailPanel_employeeId\").innerHTML=emp.employeeId;");
+pw.println("document.getElementById(\"detailPanel_name\").innerHTML=emp.name;");
+pw.println("document.getElementById(\"detailPanel_designation\").innerHTML=emp.designation;");
+pw.println("document.getElementById(\"detailPanel_dateOfBirth\").innerHTML=emp.dateOfBirth;");
+pw.println("document.getElementById(\"detailPanel_gender\").innerHTML=emp.gender;");
+pw.println("if(emp.isIndian)");
+pw.println("{");
+pw.println("document.getElementById(\"detailPanel_isIndian\").innerHTML=\"Yes\";");
+pw.println("}");
+pw.println("else");
+pw.println("{");
+pw.println("document.getElementById(\"detailPanel_isIndian\").innerHTML=\"No\";");
+pw.println("}");
+pw.println("document.getElementById(\"detailPanel_basicSalary\").innerHTML=emp.basicSalary;");
+pw.println("document.getElementById(\"detailPanel_panNumber\").innerHTML=emp.panNumber;");
+pw.println("document.getElementById(\"detailPanel_aadharCardNumber\").innerHTML=emp.aadharCardNumber;");
+pw.println("}");
+pw.println("</script>");
+pw.println("</head>");
+pw.println("<body>");
+pw.println("<!-- Main container starts here -->");
+pw.println("<div style='width:90hw;height:auto;border:1px solid black'>");
+pw.println("<!-- header starts here -->");
+pw.println("<div style='margin:5px;width:90hw;height:auto;border:1px solid black'>");
+pw.println("<a href='/WebApplication1/index.html'><img src='/WebApplication1/images/logo.png' style='float:left;width:7hw;height:7vh'></a>");
+pw.println("<div style='margin-top:9px;margin-bottom:9px;font-size:20pt'>HR Application</div>");
+pw.println("</div>");
+pw.println("<!-- header ends here-->");
+pw.println("<!--content-section starts here -->");
+pw.println("<div style='width:90hw;height:70vh;margin:5px;border:1px solid white'>");
+pw.println("<!--left panel starts here -->");
+pw.println("<div style='height:65vh;margin:5px;float:left;padding:5px;border:1px solid black'>");
+pw.println("<a href='/WebApplication1/designationsView'>Designations</a></br>");
+pw.println("<b>Employees</b></br>");
+pw.println("<a href='/WebApplication1/index.html'>Home</a>");
+pw.println("</div>");
+pw.println("<!--left panel ends here -->");
+pw.println("<!--right panel starts here -->");
+pw.println("<div style='height:65vh;margin-left:5px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;border:1px solid black'>");
+pw.println("");
+pw.println("");
+pw.println("<div style='height:40vh;margin-left:5px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;border:1px solid black;overflow:scroll'>");
+pw.println("<h2>Employees</h2>");
+pw.println("<table border='1'>");
+pw.println("<thead>");
+pw.println("<tr>");
+pw.println("<th colspan='6' style='text-align:right;padding:5px'>");
+pw.println("<a href='/WebApplication1/getEmployeeAddForm'>Add Employee</a>");
+pw.println("</th>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<th style='width:60px;text-align:center'>S.No.</th>");
+pw.println("<th style='width:60px;text-align:center'>Id.</th>");
+pw.println("<th style='width:200px;text-align:center'>Name</th>");
+pw.println("<th style='width:200px;text-align:center'>Designation</th>");
+pw.println("<th style='width:100px;text-align:center'>Edit</th>");
+pw.println("<th style='width:100px;text-align:center'>Delete</th>");
+pw.println("</tr>");
+pw.println("</thead>");
+pw.println("<tbody>");
+
+
+int sno=0;
+for(EmployeeDTO employee:employees)
+{
+sno++;
+pw.println("<tr style='cursor:pointer' onclick='selectEmployee(this,\""+employee.getEmployeeId()+"\")'>");
+pw.println("<td style='text-align:right'>"+sno+"</td>");
+pw.println("<td>"+employee.getEmployeeId()+"</td>");
+pw.println("<td>"+employee.getName()+"</td>");
+pw.println("<td>"+employee.getDesignation()+"</td>");
+pw.println("<td style='text-align:center'><a href='/WebApplication1/editEmployee?employeeId="+employee.getEmployeeId()+"'>Edit</a></td>");
+pw.println("<td style='text-align:center'><a href='/WebApplication1/confirmDeleteEmployee?employeeId="+employee.getEmployeeId()+"'>Delete</a></td>");
+pw.println("</tr>");
+}
+
+
+
+pw.println("</tbody>");
+pw.println("</table>");
+pw.println("</div>");
+pw.println("");
+pw.println("");
+pw.println("<div style='height:18vh;margin-left:95px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;border:1px solid black'>");
+pw.println("<label style='background:grey;color:white;padding-left:5px;padding-right:5px'>Details</label>");
+pw.println("<table border='0' width='100%'>");
+pw.println("<tr>");
+pw.println("<td>Employee Id : <span id='detailPanel_employeeId' style='margin-right:40px'></span></td>");
+pw.println("<td>Name : <span id='detailPanel_name' style='margin-right:40px'></span></td>");
+pw.println("<td>Designation : <span id='detailPanel_designation' style='margin-right:40px'></span></td>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<td>DateOfBirth : <span id='detailPanel_dateOfBirth' style='margin-right:40px'></span></td>");
+pw.println("<td>Gender : <span id='detailPanel_gender' style='margin-right:40px'></span></td>");
+pw.println("<td>isIndian : <span id='detailPanel_isIndian' style='margin-right:40px'></span></td>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<td>Basic Salary : <span id='detailPanel_basicSalary' style='margin-right:40px'></span></td>");
+pw.println("<td>PanNumber : <span id='detailPanel_panNumber' style='margin-right:40px'></span></td>");
+pw.println("<td>Aadhar Card Number : <span id='detailPanel_aadharCardNumber' style='margin-right:40px'></span></td>");
+pw.println("</tr>");
+pw.println("</table>");
+pw.println(" ");
+pw.println("</div>");
+pw.println("</div>");
+pw.println("");
+pw.println("<!-- right panel ends here -->");
+pw.println("</div>");
+pw.println("<!--content-section ends here-->");
+pw.println("<!--footer starts here -->");
+pw.println("<div style='width:90hw;height:auto;margin:5px;text-align:center;border:1px solid white'>&copy; Mayuri Mahto 2025</div>");
+pw.println("<!--footer ends here-->");
+pw.println("</div>");
+pw.println("<!--Main container ends here-->");
+pw.println("</body>");
+pw.println("</html>");
+		}
+		catch(Exception exception)
+		{
+			System.out.println(exception.getMessage());
+		}
+
 	}
 		
 }
